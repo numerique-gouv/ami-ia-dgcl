075060daf93d958c73136562be7192a619955fcd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 09:14:29 +0000

    Adding tarfile member sanitization to extractall()

--- aclia_data/pipeline/utils.py
+++ aclia_data/pipeline/utils.py
@@ -82,7 +82,26 @@ def unzip(input_path, output_path=None):
             output_path = re.sub('\.tar\.gz$', '', input_path)
     
         with tarfile.open(input_path, "r:gz") as tfile:
-            tfile.extractall(output_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tfile, output_path)
             
     elif input_path.endswith('.7z'):
         if output_path is None:
